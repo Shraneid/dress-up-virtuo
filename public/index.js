72e7ecf19521def3cfc769e3b2217bd58742e40c
quentin lap <quentin.tourette@edu.devinci.fr>
2020-01-15 08:27:00 +0100

    added materialize + modified css + html for requested values, added parallax effect and autoscroll on compute

--- public/index.js
+++ public/index.js
@@ -1,38 +1,50 @@
 /* global VIRTUO*/
 'use strict';
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, { format: "yyyy-mm-dd" });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, { "responsiveThreshold": 0 });
+});
+
 (() => {
-  const render = (actors) => {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    const template = actors.map(actor => {
-      return `
-        <div class="actor">
+    const render = (actors) => {
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement('div');
+        const template = actors.map(actor => {
+            return `
+        <div class="actor col s6 center">
           <span>${actor.who}</span>
           <span>${actor.type}</span>
           <span>${actor.amount}</span>
         </div>
       `;
-    }).join('');
+        }).join('');
 
-    div.innerHTML = template;
-    fragment.appendChild(div);
-    document.querySelector('#actors').innerHTML = '';
-    document.querySelector('#actors').appendChild(fragment);
-  };
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        document.querySelector('#actors').innerHTML = '';
+        document.querySelector('#actors').appendChild(fragment);
+    };
 
-  const button = document.querySelector('#compute');
+    const button = document.querySelector('#compute');
 
-  button.addEventListener('click', function onClick () {
-    const car = VIRTUO.getCar();
-    const begin = document.querySelector('#rental .js-begin').value;
-    const end = document.querySelector('#rental .js-end').value;
-    const distance = document.querySelector('#rental .js-distance').value;
-    const option = document.querySelector('#rental .js-option').checked;
-    const actors = VIRTUO.payActors(car, begin, end, distance, option);
+    button.addEventListener('click', function onClick() {
+        const car = VIRTUO.getCar();
+        const begin = document.querySelector('#rental .js-begin').value;
+        const end = document.querySelector('#rental .js-end').value;
+        const distance = document.querySelector('#rental .js-distance').value;
+        const option = document.querySelector('#rental .js-option').checked;
+        const actors = VIRTUO.payActors(car, begin, end, distance, option);
 
-    render(actors);
-
-    return;
-  });
+        render(actors);
+        var x = document.getElementById("actors");
+        x.style.display = "block";
+        return;
+    });
 })();
