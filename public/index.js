a0e516126aa50ce5a7bf5db1bd9c2b1b0c09eabf
quentin lap <quentin.tourette@edu.devinci.fr>
2020-01-15 17:32:30 +0100

    Added styling to results, changed the js to render price separately for ease of styling, added materialize collapsible to first hide results and show price (UX attempt)

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 /* global VIRTUO*/
 'use strict';
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, { format: "yyyy-mm-dd" });
@@ -12,24 +11,68 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Parallax.init(elems, { "responsiveThreshold": 0 });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("1");
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, null);
+});
+
+(() => {
+    const renderPrice = (actor) => {
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement('div');
+
+        const template = `
+                <div class="title topPad" id="price">
+                    <span>Price : ${actor.amount}$</span>
+                </div>
+            `;
+
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        document.querySelector('#priceDIV').innerHTML = '';
+        document.querySelector('#priceDIV').appendChild(fragment);
+    }
+});
+
 (() => {
     const render = (actors) => {
         const fragment = document.createDocumentFragment();
         const div = document.createElement('div');
+
         const template = actors.map(actor => {
-            return `
-        <div class="actor col s6 center">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
-        </div>
-      `;
+            if (actor == actors[0]) {
+                return
+            } else {
+                return `
+                    <div class="priceInfo">
+                        <span>${actor.who}</span>
+                        <span>${actor.amount}$</span>
+                    </div>
+                  `;
+            }
         }).join('');
 
         div.innerHTML = template;
         fragment.appendChild(div);
         document.querySelector('#actors').innerHTML = '';
         document.querySelector('#actors').appendChild(fragment);
+    };
+
+    const renderPrice = (actor) => {
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement('div');
+
+        const template = `
+                <div class="title topPad" id="price">
+                    <span>Price : ${actor.amount}$</span>
+                </div>
+            `;
+
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        document.querySelector('#priceDIV').innerHTML = '';
+        document.querySelector('#priceDIV').appendChild(fragment);
     };
 
     const button = document.querySelector('#compute');
@@ -42,9 +85,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const option = document.querySelector('#rental .js-option').checked;
         const actors = VIRTUO.payActors(car, begin, end, distance, option);
 
+        renderPrice(actors[0]);
         render(actors);
-        var x = document.getElementById("actors");
-        x.style.display = "inline-block";
+        var x = document.getElementById("target");
+        x.style.display = "block";
         return;
     });
 })();
